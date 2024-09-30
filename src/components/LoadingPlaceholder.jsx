8c8eedffde82d15385d11943ff2598bcec6d4bd2
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-09-30 19:04:09 +0200

    add transition and keyframes

--- src/components/LoadingPlaceholder.jsx
+++ src/components/LoadingPlaceholder.jsx
@@ -3,13 +3,15 @@ import "./CSS/LoadingPlaceholder.css";
 
 const LoadingPlaceholder = () => {
   return (
-    <div className="loading-placeholder">
-      <div className="placeholder-title"></div>
-      <div className="placeholder-upvotes">
-        <div className="upvote-bar"></div>
+    <div className="container">
+      <div className="loading-placeholder">
+        <div className="placeholder-title"></div>
+        <div className="placeholder-upvotes">
+          <div className="upvote-bar"></div>
+        </div>
+        <div className="placeholder-image"></div>
+        <div className="placeholder-info"></div>
       </div>
-      <div className="placeholder-image"></div>
-      <div className="placeholder-info"></div>
     </div>
   );
 };
