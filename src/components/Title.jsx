73a4da154ad8401c8ac56cb3f5f1f4fe299f4f02
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-10-08 15:49:12 +0200

    new normal

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import "./CSS/Title.css";  
-import CodecademyLogo from "../Images/codecademy-logo1.png";
-import RedditLogo from "../Images/Reddit-logo.webp";
+import "./CSS/Title.css";
+import CodecademyLogo from "../Images/codecademy-icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReddit } from "@fortawesome/free-brands-svg-icons";
 
 const Title = () => {
   return (
     <div className="icons">
-      <a href="https://www.codecademy.com" target="_blank" rel="noopener noreferrer">
-        <img className="Codecademy-logo" src={CodecademyLogo} alt="Codecademy logo"/>
+      <a
+        href="https://www.codecademy.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="Codecademy-logo"
+          src={CodecademyLogo}
+          alt="Codecademy logo"
+        />
       </a>
-      <a href="https://www.reddit.com" target="_blank" rel="noopener noreferrer">
-        <img className="Reddit-logo" src={RedditLogo} alt="Reddit logo"/>
+      <a
+        href="https://www.reddit.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faReddit}
+          className="reddit-icon"
+          alt="Reddit logo"
+          style={{ color: "#f55019" }}
+        />
       </a>
     </div>
   );
